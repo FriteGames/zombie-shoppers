25a852c9709e5b11f724e0526e737030d42a63a0
zachlite <zachlite@gmail.com>
2018-04-20 21:38:55 -0400

    no more dispatch queue

--- src/dispatch.ts
+++ src/dispatch.ts
@@ -30,25 +30,8 @@ const initialState: State = {
 
 let state: State = initialState;
 
-let actionQueue = [];
-
 export default function dispatch(action) {
-  actionQueue.push(action);
-  if (actionQueue.length === 1) {
-    runActions(actionQueue);
-  }
-}
-
-function runActions(pending) {
-  pending.forEach(action => {
-    state = reducer(state, action);
-    actionQueue.shift();
-  });
-
-  // run whatever has been added to the queue since the last invocation
-  if (actionQueue.length > 0) {
-    runActions(actionQueue);
-  }
+  state = reducer(state, action);
 }
 
 export function getState() {
